60b73e19a067e632756b92014a11711a12ca8b5a
frantisek-boura <frantisekboura2001@gmail.com>
2026-07-30 17:19:16 +0200

    init: first commit, clean project

---
+++ CliTaskTrackerApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clitracker
+{
+    public class CliTaskTrackerApp 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    } 
+}
